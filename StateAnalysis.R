2f491259b1167e24afd251ef01b185253db5c680
Abelardosalazar <jesus.15.08.98@gmail.com>
2022-11-03 15:49:55 -0600

    updateR03/11/22

--- StateAnalysis.R
+++ StateAnalysis.R
@@ -1,2 +1,22 @@
-setwd('C:\Users\j_abe\Documents\DataScienceProyects\CrimeInMexico\Resources')
-crime<-read.csv(file="Crime_Mexico.csv")
+getwd()
+#Tiene que ser a fuerzas entre""
+setwd("C:/Users/j_abe/Documents/DataScienceProyects/CrimesMexico/Resources")
+crime<-read.csv(file="Crime_Mexico.csv",sep="\t")
+head(crime)
+
+#Primero ver una generalización de los datos 
+unique(crime$State)
+unique(crime$Tipo.de.delito)
+unique(crime$Year)
+crime$December<-as.numeric(crime$December)
+
+crime$EneroDiciembre<-(crime$January+crime$February+crime$March+crime$April+crime$May+crime$June+crime$July+crime$August+crime$September+crime$October+crime$November+crime$December)
+
+x<-apply(crime, 2, function(x) length(unique(x)))
+table(crime$State)
+length(crime$Tipo.de.delito)
+
+
+ggplot(data = crime) +
+  geom_bar(mapping = aes(x =crime$Tipo.de.delito))+
+  theme(axis.text.x = element_text(angle = 90, size = 10))
